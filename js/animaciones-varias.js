c54ab378fec68d8d5838bee098f77b04ac341e0a
Grytcarx <g.c.rz159@gmail.com>
2021-07-21 21:55:20 -0500

    Agrega animaciones a los titulos de las secciones

---
+++ js/animaciones-varias.js
@@ -0,0 +1,96 @@
+// Animacion para el "h3" sobre mi
+const h3SobreMi = document.getElementById("h3-sobre-mi");
+h3SobreMi.style.cursor = "pointer";
+let h3SobreMiActivo = false;
+
+async function H3SobreMi(){
+    if(!h3SobreMiActivo) {
+
+        h3SobreMiActivo = true;
+        let velocidad = 30;
+        let cantidad = 10;
+        let simbolo = "-";
+        let aux = "";
+        
+        for(let i=0; i<cantidad; i++){
+            aux+=simbolo;
+            h3SobreMi.innerText= `Sobre ${aux} mi`;
+            await Esperar(velocidad);
+        }
+        while(aux!="-"){
+            aux=aux.substring(0, aux.length-1);
+            h3SobreMi.innerText= `Sobre ${aux} mi`;
+            await Esperar(velocidad);
+        }
+
+        h3SobreMi.innerText= `Sobre mi`;
+        h3SobreMiActivo = false;
+    }
+    
+}
+
+h3SobreMi.addEventListener('click', H3SobreMi);
+
+
+
+
+
+
+// Animacion para el "h3" mis skills
+const h3MisSkills = document.getElementById("h3-mis-skills");
+h3MisSkills.style.cursor = "pointer";
+let h3MisSkillsActivo = false;
+
+async function H3MisSkills(){
+    if(!h3MisSkillsActivo) {
+        h3MisSkillsActivo = true;
+        let velocidad = 70;
+    
+        let textoOriginal = h3MisSkills.innerText;
+        let texto = textoOriginal.split("");
+    
+        for(let i=1; i<texto.length; i++){
+            if(texto[i]!=" "){
+                texto[i] = texto[i].toUpperCase();
+            }
+            h3MisSkills.innerText= texto.join("");
+            await Esperar(velocidad);
+        }
+    
+        for(let i=0; i<texto.length; i++){
+            if(texto[i]!=" "){
+                texto[i] = texto[i].toLowerCase();
+            }
+            h3MisSkills.innerText= texto.join("");
+            await Esperar(velocidad);
+        }
+    
+        h3MisSkills.innerText= textoOriginal;
+        h3MisSkillsActivo = false;
+    }
+}
+
+h3MisSkills.addEventListener('click',H3MisSkills);
+
+
+
+
+
+const h3MisProyectos = document.getElementById("h3-mis-proyectos");
+h3MisProyectos.style.cursor = "pointer";
+let h3MisProyectosActivo = false;
+
+async function H3MisProyectos() {
+    if(!h3MisProyectosActivo) {
+        h3MisProyectosActivo = true;
+
+        h3MisProyectos.style.transition = "font-size .5s";
+        h3MisProyectos.style.fontSize = "3rem";
+        await Esperar(500);
+        h3MisProyectos.style.fontSize = "2.2rem";
+        await Esperar(500);
+        h3MisProyectosActivo = false;
+    }
+}
+
+h3MisProyectos.addEventListener('click',H3MisProyectos);
